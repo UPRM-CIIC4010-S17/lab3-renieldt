541c4ef10b68261787acfc05a77106c632750810
4010d2 <4010d2@aguacate.ece.uprm.edu>
2017-02-08 14:26:17 -0400

    Changed the frame name

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame Reniel = new JFrame("Hello, world!");
-		Reniel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		Reniel.setLocation(900, 400);
-		Reniel.setSize(200, 200);
-		Reniel.setVisible(true);
+		JFrame myFrame = new JFrame("Reniel Irizarry");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(900, 400);
+		myFrame.setSize(200, 200);
+		myFrame.setVisible(true);
 	}
 }
